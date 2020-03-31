4f2772bf46ff153f39b74b1567c3cb2d4d202a2c
mitchya1 <mitchell.anderson@colorado.edu>
2020-03-31 15:08:14 -0600

    reorg

--- src/v1/k8s15/main.go
+++ src/v1/k8s15/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -95,53 +94,4 @@ func main() {
 		//log.Println("CPU limit is", deploymentInfo.Spec.Template.Spec.Containers[k].Resources.Limits.CPU)
 		//log.Println("Memory limit is", deploymentInfo.Spec.Template.Spec.Containers[k].Resources.Limits.Memory)
 	}
-}
-
-func parseCPUReading(cpu string) (int, string, error) {
-	unit := string(cpu[len(cpu)-1])
-	cpuStr := cpu[0 : len(cpu)-1]
-	cpuInt, err := strconv.Atoi(cpuStr)
-
-	if err != nil {
-		log.Println("Unable to convert", cpuStr, "to int")
-		log.Println(err.Error())
-		return 0, "", err
-	}
-
-	return cpuInt, unit, nil
-}
-
-func convertCPUWrapper(cpuUsage int, cpuUnit string) (int, string) {
-	switch cpuUnit {
-	case "n":
-		return convertNanoToMilli(cpuUsage), "milliCPU"
-	case "m":
-		return cpuUsage, "milliCPU"
-	}
-
-	// This should never get called
-	return cpuUsage, "milliCPU"
-}
-
-func parseCPULimit(cpuLimit string) int {
-	var wrappedCPULimit int
-	wrappedCPULimit, err = strconv.Atoi(cpuLimit)
-
-	if err != nil {
-		log.Println("Error converting", cpuLimit, "to int. Assuming CPU limit specifies millicpu")
-		cpuStr := cpuLimit[0 : len(cpuLimit)-1]
-		wrappedCPULimit, err = strconv.Atoi(cpuStr)
-	} else {
-		wrappedCPULimit = wrappedCPULimit * 1000
-	}
-
-	return wrappedCPULimit
-}
-func convertNanoToMilli(cpuUsage int) int {
-	converted := cpuUsage / 1000000
-	return converted
-}
-
-func convertKibiToMibi() {
-
 }
