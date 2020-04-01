fc5784336604d8941462d573298cde49aece20e8
mitchya1 <mitchell.anderson@colorado.edu>
2020-04-01 12:32:27 -0600

    add dockerfile and build helper

---
+++ helpers/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker run --rm \
+-v ${PWD}:/go/src/github.com/mitchya1/sanepa:Z \
+-w /go/src/github.com/mitchya1/sanepa \
+-e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 \
+golang:1.13.9 go build -v -o sanepa ./src/v1/k8s15
