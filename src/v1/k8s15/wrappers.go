4aef0d37f953a348faf6d14d3bc9962839432228
mitchya1 <mitchell.anderson@colorado.edu>
2020-04-30 14:17:56 -0600

    clear metricState when reset scaling counters

--- src/v1/k8s15/wrappers.go
+++ src/v1/k8s15/wrappers.go
@@ -107,4 +107,5 @@ func generateThreshold(limit int, threshold int) int {
 func resetScalingCounters() {
 	shouldScaleDownCounter = 0
 	shouldScaleUpCounter = 0
+	metricState = make(map[string]metricReadings)
 }
